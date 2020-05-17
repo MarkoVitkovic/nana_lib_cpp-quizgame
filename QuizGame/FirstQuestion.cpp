88b54316f03f75bf747dd18e4e3d91f0fb9a6c5a
Marko Vitković <mavitkov@vsite.hr>
2020-05-17 23:07:57 +0200

    add backround image on first window, also make buttons transparent

--- QuizGame/FirstQuestion.cpp
+++ QuizGame/FirstQuestion.cpp
@@ -2,6 +2,8 @@
 #include <nana/gui/widgets/label.hpp>
 #include <nana/gui/widgets/button.hpp>
 #include <nana/gui/msgbox.hpp>
+#include <nana/gui/place.hpp>
+
 
 int main()
 {
@@ -11,19 +13,21 @@ int main()
 	form fm;
 	form sec;
 	form thrid;
-	nana::API::track_window_size(fm, { 500,400 }, false); //minimum
-	nana::API::track_window_size(fm, { 700,600 }, true); //maximum
+
+	//Forms size
+	nana::API::track_window_size(fm, { 500,350 }, false); //minimum
+	nana::API::track_window_size(fm, { 500,350 }, true); //maximum
 	nana::API::track_window_size(sec, { 700,400 }, false); //minimum
 	nana::API::track_window_size(sec, { 1000,600 }, true); //maximum
-	fm.caption("QuizGame");
+	fm.caption("Trivia Night");
+	
 
 	//Define a label and display a text.
-	label lab{ fm, "<bold size=30>Quiz Game</>" };
-	lab.text_align(align::center);
 	label question{ sec, "<bold size=30>Tko je Rus?</>" };
+	API::effects_bground(question, effects::bground_transparent(0), 0);
 	question.text_align(align::center);
-	lab.format(true);
 	question.format(true);
+	
 
 	//Define a msgbox
 	msgbox m(sec, "Wrong answer", msgbox::ok);
@@ -31,18 +35,24 @@ int main()
 	msgbox mc(sec, "Correct answer", msgbox::ok);
 	mc << "Correct answer, nice!";
 
-	//Define a button and answer the click event.
-	button btn{ fm, "Quit" };
-	btn.events().click([&fm] {
-		fm.close();
-	});
-	button btn2{ fm, "Start" };
-	btn2.events().click([&sec, &fm] {
+
+	//Define a button and answer the click event(main form).
+	button btn{ fm, nana::rectangle(130, 270, 100 , 30) };
+	btn.events().click([&sec, &fm] {
 		sec.show();
+	});
+	btn.caption("Start"); 
+	API::effects_bground(btn, effects::bground_transparent(0), 0);
+	btn.fgcolor(colors::white);
+
+	button btn2{ fm, nana::rectangle(280, 270, 100, 30)};
+	btn2.events().click([&fm] {
 		fm.close();
 	});
-	btn2.bgcolor(color_rgb(0x7ACC));
+	btn2.caption("Quit");
+	API::effects_bground(btn2, effects::bground_transparent(0), 0);
 	btn2.fgcolor(colors::white);
+
 
 	//Define a button on second form
 	button q1{ sec, "Covjek" };
@@ -56,7 +66,7 @@ int main()
 		m.show();
 	});
 	button q3{ sec, "Konj" };
-	q3.events().click([&sec, &m] {
+	q3.events().click([&sec, &m, &fm] {
 		m.show();
 	});
 	button q4{ sec, "Zmaj" };
@@ -69,22 +79,35 @@ int main()
 	});
 
 	//Layout management
-	fm.div("vert <><<><weight=100% text><>><><weight=30<><button><>><>");
-	fm["text"] << lab;
-	fm["button"] << btn2;
-	fm["button"] << btn;
-	fm.collocate();
 	sec.div("vert <><<><weight=100% text><>><><weight=30<><button><>><>");
 	sec["text"] << question;
-	sec["button"] << q1;
-	sec["button"] << q2;
-	sec["button"] << q3;
-	sec["button"] << q4;
+	sec["button"] << q1 << q2;
+	sec["button"] << q3 << q4;
 	sec["button"] << quit;
 	sec.collocate();
 
+
+	
 	//Show the form
 	fm.show();
+
+	nana::paint::image mainImg("C:\\Users\\Korisnik\\source\\repos\\QuizGame\\QuizGame\\firstpage.jpg");
+	drawing dw(fm);
+	dw.draw([&mainImg](nana::paint::graphics & graph)
+	{
+		if (mainImg.empty()) return;
+		mainImg.paste(graph, nana::point{});
+	});
+	dw.update();
+
+	nana::paint::image Img("C:\\Users\\Korisnik\\source\\repos\\QuizGame\\QuizGame\\Question.jpg");
+	drawing dw1(sec);
+	dw1.draw([&Img](nana::paint::graphics & graph)
+	{
+		if (Img.empty()) return;
+		Img.paste(graph, nana::point{});
+	});
+	dw1.update();
 
 	//Start to event loop process, it blocks until the form is closed.
 	exec();
