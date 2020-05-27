efffe51fc128b8d540b04b80253d0ed1790b20c3
Marko Vitković <mavitkov@vsite.hr>
2020-05-27 21:27:34 +0200

    add new form

--- QuizGame/TriviaNight.cpp
+++ QuizGame/TriviaNight.cpp
@@ -12,14 +12,21 @@ int main()
 	form fm;
 	form questionOne;
 	form questionTwo;
+	form questionThree;
 
 	//Forms size
 	nana::API::track_window_size(fm, { 500,350 }, false); //minimum
 	nana::API::track_window_size(fm, { 500,350 }, true); //maximum
 	nana::API::track_window_size(questionOne, { 700,450 }, false); //minimum
 	nana::API::track_window_size(questionOne, { 700,450 }, true); //maximum
+	nana::API::track_window_size(questionTwo, { 700,450 }, false); //minimum
+	nana::API::track_window_size(questionTwo, { 700,450 }, true); //maximum
+	nana::API::track_window_size(questionThree, { 700,450 }, false); //minimum
+	nana::API::track_window_size(questionThree, { 700,450 }, true); //maximum
 	fm.caption("Trivia Night");
 	questionOne.caption("Trivia Night");
+	questionTwo.caption("Trivia Night");
+	questionThree.caption("Trivia Night");
 	
 
 	//Define a label and display a text.
@@ -32,10 +39,14 @@ int main()
 	
 
 	//Define a msgbox
-	msgbox m(questionOne, "Wrong answer", msgbox::ok);
+	msgbox m(fm, "Wrong answer", msgbox::ok);
 	m << "Wrong answer, try again.";
 	msgbox mc(questionOne, "Correct answer", msgbox::ok);
 	mc << "Correct answer, nice!";
+	nana::msgbox correct{ "Correct answer" };
+	correct << "Correct answer, nice!";
+	nana::msgbox wrong{ "Wrong answer" };
+	wrong << "Wrong answer, try again.";
 
 
 	//Define a button and answer the click event(main form).
@@ -58,36 +69,73 @@ int main()
 
 	//Define a button on questionOne form
 	button q1a1{ questionOne, nana::rectangle(145, 270, 200 , 40) };
-	q1a1.events().click([&questionOne, &fm] {
-		questionOne.show();
+	q1a1.events().click([&questionOne, &correct, &questionTwo] {
+		correct.show();
+		questionTwo.show();
+		questionOne.close();
 	});
-	q1a1.caption("Rus");
+	q1a1.caption("Man");
 	API::effects_bground(q1a1, effects::bground_transparent(0), 0);
 	q1a1.fgcolor(colors::white);
 
 	button q1a2{ questionOne, nana::rectangle(365, 270, 200 , 40) };
-	q1a2.events().click([&questionOne, &fm] {
-		questionOne.show();
+	q1a2.events().click([&wrong] {
+		wrong.show();
 	});
 	q1a2.caption("Rus");
 	API::effects_bground(q1a2, effects::bground_transparent(0), 0);
 	q1a2.fgcolor(colors::white);
 
 	button q1a3{ questionOne, nana::rectangle(145, 330, 200 , 40) };
-	q1a3.events().click([&questionOne, &fm] {
-		questionOne.show();
+	q1a3.events().click([&wrong] {
+		wrong.show();
 	});
 	q1a3.caption("Rus");
 	API::effects_bground(q1a3, effects::bground_transparent(0), 0);
 	q1a3.fgcolor(colors::white);
 
 	button q1a4{ questionOne, nana::rectangle(365, 330, 200 , 40) };
-	q1a4.events().click([&questionOne, &fm] {
-		questionOne.show();
+	q1a4.events().click([&wrong] {
+		wrong.show();
 	});
 	q1a4.caption("Rus");
 	API::effects_bground(q1a4, effects::bground_transparent(0), 0);
 	q1a4.fgcolor(colors::white);
+
+	//Define a button on questionTwo form
+	button q2a1{ questionTwo, nana::rectangle(145, 270, 200 , 40) };
+	q2a1.events().click([&questionTwo, &correct, &questionThree] {
+		correct.show();
+		questionThree.show();
+		questionTwo.close();
+	});
+	q2a1.caption("Man");
+	API::effects_bground(q2a1, effects::bground_transparent(0), 0);
+	q2a1.fgcolor(colors::white);
+
+	button q2a2{ questionTwo, nana::rectangle(365, 270, 200 , 40) };
+	q2a2.events().click([&wrong] {
+		wrong.show();
+	});
+	q2a2.caption("Rus");
+	API::effects_bground(q2a2, effects::bground_transparent(0), 0);
+	q2a2.fgcolor(colors::white);
+
+	button q2a3{ questionTwo, nana::rectangle(145, 330, 200 , 40) };
+	q2a3.events().click([&wrong] {
+		wrong.show();
+	});
+	q2a3.caption("Rus");
+	API::effects_bground(q2a3, effects::bground_transparent(0), 0);
+	q2a3.fgcolor(colors::white);
+
+	button q2a4{ questionTwo, nana::rectangle(365, 330, 200 , 40) };
+	q1a4.events().click([&wrong] {
+		wrong.show();
+	});
+	q2a4.caption("Rus");
+	API::effects_bground(q2a4, effects::bground_transparent(0), 0);
+	q2a4.fgcolor(colors::white);
 	
 
 	//Show the form
@@ -111,6 +159,22 @@ int main()
 		Img.paste(graph, nana::point{});
 	});
 	dw1.update();
+
+	drawing dw2(questionTwo);
+	dw2.draw([&Img](nana::paint::graphics & graph)
+	{
+		if (Img.empty()) return;
+		Img.paste(graph, nana::point{});
+	});
+	dw2.update();
+
+	drawing dw3(questionThree);
+	dw3.draw([&Img](nana::paint::graphics & graph)
+	{
+		if (Img.empty()) return;
+		Img.paste(graph, nana::point{});
+	});
+	dw3.update();
 
 	//Start to event loop process, it blocks until the form is closed.
 	exec();
