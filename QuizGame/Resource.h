df2932efc5e2dd9932dadea9b3b9202871df8228
Marko Vitković <mavitkov@vsite.hr>
2020-07-18 16:51:17 +0200

    fixing some issues with questions

--- QuizGame/Resource.h
+++ QuizGame/Resource.h
@@ -17,10 +17,10 @@
 
 
 //Questions
-#define QUESTION_ONE	"<size=25>Which of these states admitted Croatia?</>"
+#define QUESTION_ONE	"<size=25>Which state first admit Croatia?</>"
 #define QUESTION_TWO	"<size=25>If Logx (1 / 8) = - 3 / 2, then x is equal to?</>"
 #define QUESTION_THREE	"<size=25>What is name of big yellow cat wich love eat lassagne?</>"
-#define QUESTION_FOUR	"<size=25>Which of the following is a symbol of London?</>"
+#define QUESTION_FOUR	"<size=25>What is a symbol of London?</>"
 #define QUESTION_FIVE	"<size=25>Who scored only goal in 2014 World Cup final?</>"
 #define QUESTION_SIX	"<size=25>In which year the Spanish flu appeared?</>"
 #define QUESTION_SEVEN	"<size=25>Who is author of Harry Potter book?</>"
@@ -34,12 +34,12 @@
 #define ANSWER_TWO		"4"
 #define ANSWER_THREE	"Garfield"
 #define ANSWER_FOUR		"Big Ben"
-#define ANSWER_FIVE		"Gotze"
+#define ANSWER_FIVE		"M.Gotze"
 #define ANSWER_SIX		"1918"
 #define ANSWER_SEVEN	"J.K.Rowling"
-#define ANSWER_EIGHT	"Eight"
-#define ANSWER_NINE		"Elvis Presley"
-#define ANSWER_TEN		"Charles Babbage"
+#define ANSWER_EIGHT	"8"
+#define ANSWER_NINE		"E.Presley"
+#define ANSWER_TEN		"C.Babbage"
 
 
 
